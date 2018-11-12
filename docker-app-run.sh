a41489cc910ed96b51667693c2c1ac6a8e42e712
atmosmaciel <atmos.mps@gmail.com>
2018-11-12 10:32:54 -0300

    atualizando contexto de criacao do Dockerfile e requirments.txt

--- docker-app-run.sh
+++ docker-app-run.sh
@@ -4,32 +4,42 @@ then
     echo "Parâmetro não definido, ou incorreto, por favor utilize '--help', para ver as opções."
 elif [ $1 == "--help" ]
 then
-    echo "Para iniciar em modo de Desenvolvimento."
-    echo "  command:    ./docker-app-run.sh --dev"
+    echo "Para iniciar em modo de Desenvolvimento, execute:"
+    echo "      ./docker-app-run.sh --dev"
     echo ""
-    echo "Para iniciar ou fazer deploy em Produção."
-    echo "  command:    ./docker-app-run.sh --prod"
+    echo "Para iniciar ou fazer deploy em Produção, execute:"
+    echo "      ./docker-app-run.sh --prod"
 elif [ $1 == "--dev" ]
 then
-    echo "#---------------ATENÇÃO---------------#"
     echo "Iniciando em modo de desenvolvimento..."
+    echo ""
+    echo "Copiando 'requirements.txt' para contexto de construcao do Dockerfile"
+    cp requirements.txt ./docker
     echo ""
     echo "Desconstruindo containers, caso existam..."
     docker-compose --project-directory . -f ./docker/docker-compose-dev.yml down
     echo ""
     echo "Construindo containers de desenvolvimento..."
-    docker-compose --project-directory . -f ./docker/docker-compose-dev.yml up
+    docker-compose --project-directory . -f ./docker/docker-compose-dev.yml up -d --build
+    echo ""
+    echo "Removendo 'requirements.txt' do contexto de construcao do Dockerfile"
+    rm ./docker/requirements.txt
 
 elif [ $1 == "--prod" ]
 then
     echo "#---------------ATENÇÃO---------------#"
     echo "Fazendo deploy em ambiente de Produção."
     echo ""
+    echo "Copiando 'requirements.txt' para contexto de construcao do Dockerfile"
+    cp requirements.txt ./docker
     echo "Desconstruindo containers, caso existam..."
     docker-compose --project-directory . -f ./docker/docker-compose-prod.yml down
     echo ""
     echo "Construindo containers de Produção."
     docker-compose --project-directory . -f ./docker/docker-compose-prod.yml up -d --build
+    echo ""
+    echo "Removendo 'requirements.txt' do contexto de construcao do Dockerfile"
+    rm ./docker/requirements.txt
 
 # Define outras opções para manipulação de containers
 elif [ $1 == "--stop-all-containers" ]
