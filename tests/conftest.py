dac84e42b37b81df76465c751ca81bbbcb40340f
Igor <igor.lima@ceuma.br>
2018-11-16 15:19:45 -0200

    Correcoes no modulo de testes da aplicacao.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-import os
 
 from app import create_app
 from app.config import TesteConfig
@@ -7,12 +6,6 @@ from app.config import TesteConfig
 
 @pytest.fixture
 def app():
-
-    settings_override = {
-        'SQLALCHEMY_DATABASE_URI': "sqlite:///%steste.db" % os.path.join(os.getcwd(), "instance/"),
-        'TESTING': True,
-        'SQLALCHEMY_TRACK_MODIFICATIONS': False
-    }
 
     app = create_app()
     app.config.from_object(TesteConfig)
--- tests/test_campus.py
+++ tests/test_campus.py
@@ -1,3 +1,4 @@
-def test_post_inscricao(client):
-    response = client.get('/campus/')
+# testa se get_campus esta sendo executado
+def test_get_campus(client):
+    response = client.get('/app-name/campus/')
     assert response is not None
