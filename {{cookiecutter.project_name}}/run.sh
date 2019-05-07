25eb64541ccb68d23e14d166f1894d3e36fd3e7d
Atmos Maciel <atmos.mps@gmail.com>
2019-05-07 20:29:31 -0300

    alterando mensagens do run.sh

--- {{cookiecutter.project_name}}/run.sh
+++ {{cookiecutter.project_name}}/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Instalando pacotes de requirements.txt necessarios a aplicacao {{cookiecutter.app_name}}..."
+echo "=> Instalando pacotes de requirements.txt necessarios a aplicacao {{cookiecutter.app_name}}..."
 pip install -r requirements.txt
-echo "Iniciando aplicacao..."
+echo ""
+echo "=> O waitress-server deu inicio ao processo de deploy da aplicação..."
 waitress-serve --call --listen=0.0.0.0:5000 app:create_app
